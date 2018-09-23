abacfd264150a6d4c94b451aea64b94cce5dfdcd
h01m3s <lin426797@gmail.com>
2018-09-22 23:17:32 -0400

    flip card

--- Memory-Card-Game/scripts.js
+++ Memory-Card-Game/scripts.js
@@ -5,6 +5,19 @@ let firstCard, secondCard;
 
 function flipCard() {
   this.classList.toggle('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
