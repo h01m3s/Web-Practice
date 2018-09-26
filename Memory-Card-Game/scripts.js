8601abd9d5237f5e007ba053203589462ca13a8c
h01m3s <lin426797@gmail.com>
2018-09-25 21:00:00 -0400

    unflipcards

--- Memory-Card-Game/scripts.js
+++ Memory-Card-Game/scripts.js
@@ -29,9 +29,16 @@ function flipCard() {
   }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
-
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 }
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
