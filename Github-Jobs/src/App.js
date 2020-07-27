62da32777be1fb6844287d95e6ff3273473b33f5
h01m3s <lin426797@gmail.com>
2020-07-27 13:04:35 -0400

    Implement Github Jobs

--- Github-Jobs/src/App.js
+++ Github-Jobs/src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+import Job from './Job';
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => ({ ...prevParams, [param]: value }));
+  };
+
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h2>Loading...</h2>}
+      {error && <h2>Error. Try Refreshing.</h2>}
+      {/* <h1>{jobs.length}</h1> */}
+      {jobs.map((job) => (
+        <Job key={job.id} job={job} />
+      ))}
+    </Container>
+  );
 }
 
 export default App;
